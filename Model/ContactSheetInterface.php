4f0c977896c6c95e416623594a5c3f1206442163
miky <mathias89100@gmail.com>
2017-06-12 14:25:02 +0200

    All

---
+++ Model/ContactSheetInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 01/12/16
+ * Time: 10:43
+ */
+
+namespace Miky\Component\Commercial\Model;
+
+
+use Miky\Component\Core\Model\CommonModelInterface;
+use Miky\Component\Core\Model\CommonModelTrait;
+use Miky\Component\Location\Model\LocationInterface;
+
+interface ContactSheetInterface
+{
+
+}
